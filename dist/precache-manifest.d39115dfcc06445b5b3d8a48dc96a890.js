c3ae38f631dae764a46fd953cc78ab369eddcb53
bipinoli <bipinoli90@gmail.com>
2020-08-10 18:37:25 +0545

    lets deploy on netlify

---
+++ dist/precache-manifest.d39115dfcc06445b5b3d8a48dc96a890.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff393afe918fe5690edd",
+    "url": "/css/app.c2cb95f7.css"
+  },
+  {
+    "revision": "0adf409733895e1bcbc74d2cc1a563ae",
+    "url": "/img/pause.0adf4097.svg"
+  },
+  {
+    "revision": "e161915968c4c870e059d0bebaebfb9b",
+    "url": "/img/resume.e1619159.svg"
+  },
+  {
+    "revision": "208a8ad84d03a9dbc08dd339132131dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff393afe918fe5690edd",
+    "url": "/js/app.c34c4fc9.js"
+  },
+  {
+    "revision": "0921e6d7d9434749cf35",
+    "url": "/js/chunk-vendors.b0b6eaba.js"
+  },
+  {
+    "revision": "09a19d8970adcc7ce8e67207f2254cdf",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
